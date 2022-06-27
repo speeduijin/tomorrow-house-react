8636cf06d361199f3b24b57d1858a2ffe651b9b0
Jeong Euijin <speeduijin91@gmail.com>
2022-06-28 07:06:09 +0900

    Refactor GnbNav

--- src/components/GnbNav.tsx
+++ src/components/GnbNav.tsx
@@ -1,19 +1,22 @@
 import './GnbNav.scss'
 
 function GnbNav() {
+  const items = ['커뮤니티', '스토어', '인테리어시공']
+
   return (
     <nav className="GnbNav sm-hidden">
       <h2 className="visually-hidden">메뉴</h2>
       <ul className="GnbNav-list">
-        <li className="GnbNav-item">
-          <a href="/">커뮤니티</a>
-        </li>
-        <li className="GnbNav-item is-active">
-          <a href="/">스토어</a>
-        </li>
-        <li className="GnbNav-item">
-          <a href="/">인테리어시공</a>
-        </li>
+        {items.map((item, idx) => {
+          return (
+            <li
+              className={`GnbNav-item ${item === '스토어' && 'is-active'}`}
+              key={idx}
+            >
+              <a href="/">{item}</a>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
