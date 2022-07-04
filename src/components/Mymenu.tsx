ae6c759b0958d86f4ea8ba167ac311c8acc18987
Jeong Euijin <speeduijin91@gmail.com>
2022-07-04 23:29:07 +0900

    Refactor MyMenu

--- src/components/Mymenu.tsx
+++ src/components/Mymenu.tsx
@@ -1,32 +1,33 @@
 import imgUser01 from '../assets/images/img-user-01.jpg'
-import './Mymenu.scss'
+import './MyMenu.scss'
 
 function Mymenu() {
+  const items = ['마이페이지', '나의 쇼핑', '이벤트', '로그아웃']
+
+  const MyMenuItem = items.map((item, idx) => {
+    return (
+      <li className="MyMenu-item" key={idx}>
+        {item === '로그아웃' ? (
+          <button type="button">{item}</button>
+        ) : (
+          <a href="/">{item}</a>
+        )}
+      </li>
+    )
+  })
+
   return (
-    <div className="Mymenu sm-hidden">
+    <div className="MyMenu sm-hidden is-active">
       <button
-        className="Mymenu-button"
+        className="MyMenu-button"
         type="button"
         aria-label="마이메뉴 열기 버튼"
       >
         <img src={imgUser01} alt="사달라 아저씨" />
       </button>
 
-      <div className="Mymenu-content">
-        <ul className="Mymenu-list">
-          <li className="Mymenu-item">
-            <a href="/">마이페이지</a>
-          </li>
-          <li className="Mymenu-item">
-            <a href="/">나의 쇼핑</a>
-          </li>
-          <li className="Mymenu-item">
-            <a href="/">이벤트</a>
-          </li>
-          <li className="Mymenu-item">
-            <button type="button">로그아웃</button>
-          </li>
-        </ul>
+      <div className="MyMenu-content">
+        <ul className="MyMenu-list">{MyMenuItem}</ul>
       </div>
     </div>
   )
