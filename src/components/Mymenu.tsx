dee0346e67e7506aa1b0d15a935d7a84bfca26a3
Jeong Euijin <speeduijin91@gmail.com>
2022-07-04 23:51:08 +0900

    Implement toggle MyMenu

--- src/components/Mymenu.tsx
+++ src/components/Mymenu.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useRef, useState } from 'react'
 import imgUser01 from '../assets/images/img-user-01.jpg'
 import './MyMenu.scss'
 
 function Mymenu() {
   const items = ['마이페이지', '나의 쇼핑', '이벤트', '로그아웃']
+
+  const [isActiveMyMenu, setIsActiveMyMenu] = useState(false)
+
+  const MyMenuRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const closeSearchHistoryOnClickingOutside = (event: MouseEvent) => {
+      MyMenuRef.current &&
+        !MyMenuRef.current.contains(event.target as Node) &&
+        setIsActiveMyMenu(false)
+    }
+
+    document.addEventListener('mousedown', closeSearchHistoryOnClickingOutside)
+    return () => {
+      document.removeEventListener(
+        'mousedown',
+        closeSearchHistoryOnClickingOutside
+      )
+    }
+  }, [MyMenuRef])
 
   const MyMenuItem = items.map((item, idx) => {
     return (
@@ -16,12 +37,19 @@ function Mymenu() {
     )
   })
 
+  const openMyMenu = () => {
+    setIsActiveMyMenu(true)
+  }
+
+  const isActive = isActiveMyMenu ? 'is-active' : ''
+
   return (
-    <div className="MyMenu sm-hidden is-active">
+    <div className={`MyMenu sm-hidden ${isActive}`} ref={MyMenuRef}>
       <button
         className="MyMenu-button"
         type="button"
         aria-label="마이메뉴 열기 버튼"
+        onClick={openMyMenu}
       >
         <img src={imgUser01} alt="사달라 아저씨" />
       </button>
