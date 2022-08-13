d51081e0a92cdd268da8dfc0364257ecb4a6a801
Jeong Euijin <speeduijin91@gmail.com>
2022-08-13 14:36:17 +0900

    Modify app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,15 @@
 import express from "express";
-import path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../../frontend/build")));
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("build"));
 
 app.get("/", (req, res) => {
-  return res.sendFile(__dirname, "../../frontend/build/index.html");
+  res.sendFile(__dirname, "/build/index.html");
 });
 
-app.listen(5000, () => {
-  console.log("listening on 5000");
+app.listen(PORT, () => {
+  console.log("server is running!");
 });
