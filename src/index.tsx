76f0401926c1b14d4d41323eade42e51e4856918
Jeong Euijin <speeduijin91@gmail.com>
2022-12-20 19:06:04 +0900

    Remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './modules/store'
 import App from './App'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
 )
